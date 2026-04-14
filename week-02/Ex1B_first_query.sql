c8b8c94bed711b3ef637182b5b4195e34ae80beb
PGHollo <pholloway0616@gmail.com>
2026-04-14 15:37:22 -0400

    added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila is a database used to collect information about films and payments for use by the store
 */
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+select * from film; -- Retrieved [number] records
+
+
