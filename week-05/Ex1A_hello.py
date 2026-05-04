b069bed1569059e962282415aa9dc8ae36d81120
PGHollo <pholloway0616@gmail.com>
2026-05-04 11:54:53 -0400

    1A python exercise

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,18 @@
 print('Hello world!')
+message = 'Hello world!'
+print(message)
+
+# "Hello world!" prints twice beacause the message is equal to "Hello World!"
+
+# Displaying dollars and cents
+
+Dollars = 3
+Cents = .50
+Cents = Cents + .25
+print(Dollars + Cents)
+
+# It combined the variables and made it into a decimal format
+
+d_str = '3 dollars'
+c_str = ' 50 cents'
+print(d_str + " " + c_str)
