a2e47fa6e55652f8b579d590974f5af4d2f9d8df
PGHollo <pholloway0616@gmail.com>
2026-05-04 15:06:44 -0400

    Class Exercise 1

---
+++ week-05/Demo1.py
@@ -0,0 +1,16 @@
+# Patrick Green-Holloway
+# 5/4/2026
+
+name = 'Patrick'
+age = 28
+
+print(name)
+print (age)
+
+print(f'My name is {name} and I am {age} years old.')
+
+pi = 3.145678
+print (f'The value of pi is {pi:.2f}')
+
+Sales = 12345.7685
+print (f'The value of pi is ${Sales:,.2f}')
