ba1934faa508eea6ec1dc747d7e49ef70a233eb5
PGHollo <pholloway0616@gmail.com>
2026-05-07 15:32:18 -0400

    Practice_Work

--- week-05/Demo1.py
+++ week-05/Demo1.py
@@ -14,3 +14,52 @@ print (f'The value of pi is {pi:.2f}')
 
 Sales = 12345.7685
 print (f'The value of pi is ${Sales:,.2f}')
+
+student = ("Alice", 20, "Data Analytics", 3.5, True)
+
+print(student)
+print(f"name : {student[0]}')")
+print(f"age : {student[1]}')")
+print(f"Major : {student[2]}')")
+print(f"GPA : {student[3]}')")
+print(f"Active : {student[4]}')")
+print(f"Length : {len(student)}")
+
+t = (1,2, 2,3,2)
+print(t.count(2))
+
+print(t.index(2))
+
+print(len(t))
+
+print(min(t))
+
+print(max(t))
+
+print(sum(t))
+
+print(sorted(t))
+
+print(type(t))
+
+t =("a", "b", "c")
+print(t[0])
+print(t[-1] )
+
+t = (1, 2, 3, 4, 5)
+print(t[1:4])
+#Concatenation
+t = (1, 2, 3)
+t2 = ("a", "b", "c")
+t3 = t + t2
+print(t3)
+
+t = (1, 2, 3)
+print(2 in t)
+
+t = (1, 2, 3, 4, 5)
+print(len(set(t)))
+
+t = (1, 2, 3, 4, 5)
+print(list(t))
+
